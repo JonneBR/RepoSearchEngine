ed3ad4a2cef9d3a664eabf8d0812938978876bf0
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2020-10-12 10:33:16 -0300

    files

---
+++ scripts/repositories.js
@@ -0,0 +1,33 @@
+function acessarRepo(repoUser, profileImage) {
+    var carregando = document.getElementById('carregando');
+    if (carregando && carregando.length) carregando.outerHTML = '';
+    var lista = document.getElementById('lista');
+    var lista_html = lista.innerHTML;
+    lista.innerHTML = '<li id="carregando">Carregando...</li>';
+  
+    axios
+      .get(repoUser)
+      .then(function (response) {
+        var objLength = response.data.length;
+        lista.innerHTML = lista_html;
+      
+  
+        var img = document.createElement('img');
+        img.src = profileImage;
+        document.getElementById('lista').appendChild(img);
+        addNameArray(profileImage);
+  
+        for (var i = 0; i < objLength; i++) {
+          var li = document.createElement('li');
+  
+          li.innerHTML = response.data[i].name;
+          addNameArray(li.innerHTML);
+          getMenuHiddenOrVisible(li.innerHTML);
+          lista.appendChild(li);
+          menuContainer[0].style.visibility = 'visible';
+        }
+      })
+      .catch(function (error) {
+        console.warn(error);
+      });
+  }
