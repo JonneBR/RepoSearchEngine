08860f559f4f417fadb1cade54686118efcd5aa2
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2020-11-09 15:50:46 -0300

    function to show users contact informations

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,48 +1,37 @@
-function contactInformations(){
-    deleteRepo();
-    var inputUserNameToLower = inputUser.value.toLowerCase();
+// function contactInformations(){
+//     deleteRepo();
+//     var inputUserNameToLower = inputUser.value.toLowerCase();
 
-  axios
-    .get('https://api.github.com/users/' + inputUserNameToLower)
-    .then(function (response) {
+//   axios
+//     .get('https://api.github.com/users/' + inputUserNameToLower)
+//     .then(function (response) {
       
-      var apiUserNameToLower = response.data.login.toLowerCase();
-      // var profileImage = response.data.avatar_url;
-      var userBlog = response.data.blog;
-      var userTwitter = response.data.twitter_username;
-      var userEmail = response.data.email;
-      console.log(userTwitter);
+//       var apiUserNameToLower = response.data.login.toLowerCase();
+//       // var profileImage = response.data.avatar_url;
+//       var userBlog = response.data.blog;
+//       var userTwitter = response.data.twitter_username;
+//       var userEmail = response.data.email;
+//       console.log(userTwitter);
 
-    saveData('Twitter: ',userTwitter);
-    saveData('Blog: ',userBlog);
-    saveData('Email: ',userEmail);
+//     saveData('Twitter: ',userTwitter);
+//     saveData('Blog: ',userBlog);
+//     saveData('Email: ',userEmail);
 
       
-    })
-    .catch(function (error) {
-      console.warn(
-        error,
-        'Não pode fazer a requisição. Usuário pode estár inválido.'
-      );
-    });
-
-
-    
-    // saveData(userBio);
-   
-   
-
-}
-
-function saveData(info,responseInformation){
-        var li = document.createElement('li');
+//     })
+//     .catch(function (error) {
+//       console.warn(
+//         error,
+//         'Não pode fazer a requisição. Usuário pode estár inválido.'
+//       );
+//     });
+// }
+// function saveData(info,responseInformation){
+//         var li = document.createElement('li');
         
-        li.innerHTML = info + responseInformation;
-        addNameArray(li.innerHTML);
-        getMenuHiddenOrVisible(li.innerHTML);
-        lista.appendChild(li);
-        menuContainer[0].style.visibility = 'visible';
-        // console.log("??", responseInformation);
-      
-
-}
+//         li.innerHTML = info + responseInformation;
+//         addNameArray(li.innerHTML);
+//         getMenuHiddenOrVisible(li.innerHTML);
+//         lista.appendChild(li);
+//         menuContainer[0].style.visibility = 'visible';
+// }
--- scripts/main.js
+++ scripts/main.js
@@ -30,6 +30,8 @@ function show(){
 
 const userRepositories = document.getElementById('user-repositories');
 
+const userContact = document.getElementById('user-contact');
+
 const name = JSON.parse(localStorage.getItem('list_names')) || [];
 
 const userProfileData = JSON.parse(localStorage.getItem('list_names')) || [];
@@ -127,7 +129,7 @@ window.addEventListener("load", () => {
   document.querySelector("#user-profile").addEventListener("click", e => {
     highligthMenuProfile(1);
     highligthMenuRepo(0);
-      userInformations()
+      userInformations();
   });
 });
 
@@ -155,3 +157,43 @@ function saveToStorage() {
   localStorage.setItem('list_names', JSON.stringify(name));
 }
 
+
+//This code is supposed to be in another file...
+
+function contactInformations(){
+  deleteRepo();
+  var inputUserNameToLower = inputUser.value.toLowerCase();
+
+axios
+  .get('https://api.github.com/users/' + inputUserNameToLower)
+  .then(function (response) {
+    
+    var apiUserNameToLower = response.data.login.toLowerCase();
+    // var profileImage = response.data.avatar_url;
+    var userBlog = response.data.blog;
+    var userTwitter = response.data.twitter_username;
+    var userEmail = response.data.email;
+    console.log(userTwitter);
+
+  saveData('Twitter: ',userTwitter);
+  saveData('Blog: ',userBlog);
+  saveData('Email: ',userEmail);
+
+    
+  })
+  .catch(function (error) {
+    console.warn(
+      error,
+      'Não pode fazer a requisição. Usuário pode estár inválido.'
+    );
+  });
+}
+function saveData(info,responseInformation){
+      var li = document.createElement('li');
+      
+      li.innerHTML = info + responseInformation;
+      addNameArray(li.innerHTML);
+      getMenuHiddenOrVisible(li.innerHTML);
+      lista.appendChild(li);
+      menuContainer[0].style.visibility = 'visible';
+}
