1dd43b8dbabc30bb380146f9d6ec75546d10f4da
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2020-11-12 15:50:46 -0300

    correct and wrong messages

--- scripts/contatoHtml.js
+++ scripts/contatoHtml.js
@@ -11,19 +11,30 @@ class UserInformations{
 
     formSubmit(){
         this.btnSubmit.onclick = event => {
-            this.infoValidation(event);
+            this.infoValidationNameField(event);
         }
     }
 
 
-    infoValidation(event){
-        if(this.nameForm.value === '' ){
+    infoValidationNameField(event){
+        if(this.nameForm.value.length === 0 ){
             event.preventDefault();
                 this.emptyInputErrorMessage(this.nameForm,`Campo não pode estar vazio!`)
-        }else{
-
+        } else if(this.nameForm.value.length >= 1 && this.nameForm.value.length <= 3 ){
+            event.preventDefault();
+                this.emptyInputErrorMessage(this.nameForm,`Campo requer mais de 3 caracteres`)
+        } 
+        else{
+            this.CorrectMessage(this.nameForm);
+            // event.preventDefault();
         }
     }
+    // infoValidationEmailField(){
+    //     if(this.emailForm.value.length <= '' ){
+    //         event.preventDefault();
+    //             this.emptyInputErrorMessage(this.emailForm,`Campo não pode estar vazio!`)
+    //     }
+    // }
 
     emptyInputErrorMessage(form,errorMessage){
         const formControl = form.parentElement;
@@ -31,6 +42,14 @@ class UserInformations{
         formControl.querySelector("input").style.border = "1px solid red";
         formControl.querySelector("small").style.visibility = 'visible';
         formControl.querySelector("small").innerText = errorMessage;
+    }
+
+    CorrectMessage(form){
+        const formControl = form.parentElement;
+        
+        formControl.querySelector("input").style.border = "1px solid green";
+        formControl.querySelector("small").style.visibility = 'hidden';
+        
     }
 }
 
